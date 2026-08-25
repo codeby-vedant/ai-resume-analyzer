f772e2a5d9ee69b1cc1b15c8dc289de04d669c33
codeby-vedant <mishravedant1079@gmail.com>
2026-08-25 23:55:43 +0530

    fix production authentication

--- Server/src/controllers/authController.js
+++ Server/src/controllers/authController.js
@@ -49,8 +49,8 @@ const loginUser = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 3 * 60 * 60 * 1000,
     });
 
--- Server/src/controllers/googleLogin.js
+++ Server/src/controllers/googleLogin.js
@@ -8,7 +8,7 @@ const googleLogin = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 3 * 60 * 60 * 1000,
     });
