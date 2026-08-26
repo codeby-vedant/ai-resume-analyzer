2b5d5a38cfbc90162059dbf3ef9554033d258b65
codeby-vedant <mishravedant1079@gmail.com>
2026-08-26 12:52:21 +0530

    fix nodemailer config

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -1,14 +1,22 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-transporter.verify()
-  .then(() => console.log("Server is ready to take our messages"))
-  .catch(err => console.error("Verification failed:", err));
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP verification failed:", error);
+  } else {
+    console.log("SMTP server is ready");
+  }
+});
+
 module.exports = transporter;
